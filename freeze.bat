bae70d7c8d415f34feda8a76e6245bbd2faffad2
unknown <graham.king@Karma0263.karma.local>
2013-04-25 17:16:48 +0100

    Fixes to freeze script to allow windows binaries.

--- freeze.bat
+++ freeze.bat
@@ -1,3 +1,3 @@
 cd src
 
-cxfreeze zoxel.py --target-dir ..\dist --base-name Win32GUI --include-modules atexit,PySide.QtNetwork,PySide.QtWebKit --include-path .
+cxfreeze zoxel.py --target-dir ..\dist --base-name Win32GUI --include-modules atexit,PySide.QtNetwork,PySide.QtWebKit,OpenGL,OpenGL.platform.win32,OpenGL.arrays.nones,OpenGL.arrays.lists,OpenGL.arrays.strings,OpenGL.arrays.numbers,OpenGL.arrays.ctypesarrays,OpenGL.arrays.ctypesparameters,OpenGL.arrays.ctypespointers --include-path .
