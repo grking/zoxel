9ff68320b67328d416c020cc3a231716686122fb
Graham King <grking.email@gmail.com>
2014-01-08 18:53:09 +0000

    Version bump 0.4.3

--- src/constants.py
+++ src/constants.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-ZOXEL_VERSION = "0.4.2 (6th January 2014)"
+ZOXEL_VERSION = "0.4.3 (8th January 2014)"
